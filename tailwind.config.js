8f4ec533222eb47e13174d9f3b95b98a38aea272
Mohamed-8 <mohamedibrahim8996@gmail.com>
2024-02-13 17:49:55 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      primary: "Poppins",
+    },
+    extend: {
+      backgroundImage : {
+        hero  : "url('./resources/mesh.png')"
+      }
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
+  },
+  plugins: [],
+}
+
